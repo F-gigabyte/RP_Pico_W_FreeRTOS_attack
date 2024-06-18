4ba2a910fbb0c8893b812324230da3da00197a8e
Fraser <ftlg987@gmail.com>
2024-06-18 12:59:37 +0100

    Added gitignore

--- example/.ycm_extra_conf.py
+++
@@ -1,4 +0,0 @@
-def Settings(**kwargs):
-    return {
-        'flags' : ['-I.', '-I/opt/pico-sdk/src/common/pico_base/include', '-I/opt/pico-sdk/src/common/pico_stdlib/include', '-I/home/fraser/rp_pico/freertos/FreeRTOS-Kernel/include', '/opt/pico-sdk/src/rp2_common/pico_cyw43_arch/include/']
-    }
--- example/build/CMakeFiles/TEST_PICO_FREERTOS.dir/opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/TEST_PICO_FREERTOS.dir/opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
- /opt/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
